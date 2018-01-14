55d8c10b02572c36ec6298f93c146b3bfe695047
knowlife4 <knowlife4@gmail.com>
2018-01-14 18:52:23 -0500

    Build 1

---
+++ Assets/HandsStick.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+ var Stuck : boolean;
+ var INPUT : String;
+function Update () {
+	if(Input.GetKeyDown(INPUT)){Stuck = true;}
+	if(!Input.GetKeyDown(INPUT)){Stuck = false;}
+}
+	
+function OnCollisionEnter(c : Collision) {
+var joint = gameObject.AddComponent(FixedJoint);
+joint.connectedBody = c.rigidbody;
+}
